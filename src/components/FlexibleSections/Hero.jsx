7b7c86288e8f8db55f76f5373f3b9d680d439c21
Tuan Nguyen <tuan.beplus@gmail.com>
2025-10-28 14:01:09 +0700

    Update event click to bounce arrow

--- src/components/FlexibleSections/Hero.jsx
+++ src/components/FlexibleSections/Hero.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import BounceArrow from '../common/BounceArrow'
 
 const HeroSection = ({ data }) => {
 
@@ -151,9 +152,7 @@ const HeroSection = ({ data }) => {
         </div>
       </div>
       { isShowBounceArrow && (
-        <div className="bounce-arrow w-16 h-16 md:w-20 md:h-20 xl:w-24 xl:h-24 absolute left-1/2 bottom-24 md:bottom-12 -translate-x-1/2 z-50 bounce">
-          <svg filter="drop-shadow(0 0 4px rgba(0, 0, 0, 0.4))" fill="#ffffff" viewBox="0 0 256 256" id="Flat" xmlns="http://www.w3.org/2000/svg" stroke="#ffffff"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M128,28A100,100,0,1,0,228,128,100.11332,100.11332,0,0,0,128,28Zm0,192a92,92,0,1,1,92-92A92.10416,92.10416,0,0,1,128,220Zm36.76953-88.76953a4,4,0,0,1,0,5.65674l-33.94141,33.9414a3.99971,3.99971,0,0,1-5.65625,0l-33.9414-33.9414a3.99992,3.99992,0,0,1,5.65674-5.65674L124,158.34326V88a4,4,0,0,1,8,0v70.34326l27.11279-27.11279A4,4,0,0,1,164.76953,131.23047Z"></path> </g></svg>
-        </div>
+        <BounceArrow/>
       )}
     </section>
   );
